8da2b18024c48a34966e16d87d6ecc3a866a7d3f
zepedrotrigo <josetrigo@ua.pt>
2022-11-10 10:29:20 +0000

    setup

---
+++ infolytics-app/src/views/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Container from '../components/Container/Container';
+
+const Home = ({ navigation }) => {
+    return (
+        <div>
+            <Container>
+                <p>Hello</p>
+            </Container >
+            <Container>
+                <p>Hello</p>
+            </Container >
+            <Container>
+                <p>Hello</p>
+            </Container >
+        </div>
+	);
+};
+
+export default Home;
