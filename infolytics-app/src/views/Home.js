226c46b607e2f7d84d1f54003eecd091a74cb1ee
Alexandre <alexandrepp07@ua.pt>
2022-11-21 23:21:58 +0000

    Update Home

--- infolytics-app/src/views/Home.js
+++ infolytics-app/src/views/Home.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import Container from '../components/Container/Container';
+import * as d3 from d3;
 
 const Home = ({ navigation }) => {
     return (
         <div>
             <Container>
-                <p>Hello</p>
+                {
+                d3.csv("marksheet.csv").then(function (data) {
+                    xScale.domain( 
+                        data.map(function (d) { 
+                            return d.name; 
+                        }) 
+                    );
+                    yScale.domain([ 
+                        0, 
+                        d3.max(data, function (d) { 
+                            return d.marks; 
+                        }), 
+                    ]); 
+                })
+                }
             </Container >
             <Container>
                 <p>Hello</p>
