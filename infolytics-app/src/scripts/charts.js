b2b3d83b7ce06c0eb8fafe3ed68caa814c9c8795
Alexandre <alexandrepp07@ua.pt>
2022-12-06 18:37:28 +0000

    "little updates"

--- infolytics-app/src/scripts/charts.js
+++ infolytics-app/src/scripts/charts.js
@@ -15,7 +15,6 @@ function horizontalBarChart() {
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-    var country = document.getElementById("country").value;
     
     // Parse the Data
     d3.csv("../edLevel_avgSalary.csv", function(data) {
@@ -100,8 +99,8 @@ function horizontalBarChart() {
 
     function onMouseOver(d, i) {
         // Get bar's xy values, ,then augment for the tooltip
-        var xPos = 1500;
-        var yPos = 25;
+        var xPos = width + margin.left;
+        var yPos = margin.bottom;
 
         // Update Tooltip's position and value
         d3.select('#tooltip')
@@ -128,9 +127,13 @@ function horizontalBarChart() {
 
 }
 
+
+
+
+
 function bar_chart_gender(){
     // set the dimensions and margins of the graph
-    var margin2 = {top: 50, right: 200, bottom: 400, left: 150},
+    var margin2 = {top: 50, right: 200, bottom: 200, left: 200},
     w2 = document.getElementById('bar_chart_gender').offsetWidth;
     h2 = document.getElementById('bar_chart_gender').offsetHeight;
     height2 = h2 - h2*(margin2.top/1000) - h2*(margin2.bottom/1000);
@@ -165,6 +168,7 @@ function bar_chart_gender(){
     .selectAll("text")
     .attr("transform", "translate(-5,0)rotate(-35)")
     .style("text-anchor", "end")
+    .style('font-size', '12px')
     .attr("fill", "grey" );
 
     // Add Y axis
@@ -176,6 +180,7 @@ function bar_chart_gender(){
     .selectAll("text")
     .attr("transform", "translate(0,0)")
     .style("text-anchor", "end")
+    .style('font-size', '12px')
     .attr("fill", "grey" );
 
     // Another scale for subgroup position?
@@ -203,40 +208,493 @@ function bar_chart_gender(){
     .attr("x", function(d) { return xSubgroup(d.key); })
     .attr("y", function(d) { return y(d.value); })
     .attr("width", xSubgroup.bandwidth())
-    .attr("height", function(d) { return height2 - y(d.value); })
+    .attr("height", function(d) { return 0; })
     .attr("fill", function(d) { return color(d.key); });
+
+    // Animation
+    svg.selectAll("rect")
+    .transition()
+    .duration(800)
+    .attr("x", function(d) { return xSubgroup(d.key); })
+    .attr("height", function(d) { return height2 - y(d.value); })
+    .delay(function(d,i){console.log(i) ; return(i*25)})
 
     })
 
     // Title
     svg.append('text')
-    .attr('x', width/3)
+    .attr('x', width2/2)
     .attr('y', -30)
     .attr('text-anchor', 'middle')
     .style('font-family', 'Helvetica')
     .style('font-size', '24px')
-    .text('Average Salary per Developer Type by Gender')
+    .text('Average Salary by Developer Type')
     .attr("fill", "white" );
 
     // X label
     svg.append('text')
-    .attr('x', width*2.65)
-    .attr('y', height)
+    .attr('x', width2 + 50)
+    .attr('y', height2 + 20)
     .attr('text-anchor', 'middle')
     .style('font-family', 'Helvetica')
-    .style('font-size', '12')
+    .style('font-size', '16')
     .text('Developer Type')
     .attr("fill", "white" );
     
     // Y label
     svg.append('text')
     .attr('text-anchor', 'middle')
-    .attr('transform', 'translate(-20,-5)rotate(0)')
+    .attr('transform', 'translate(-30,-5)rotate(0)')
     .style('font-family', 'Helvetica')
-    .style('font-size', '12')
+    .style('font-size', '16')
     .text('Average Salary')
     .attr("fill", "white" );
+
+    svg.append("circle")
+    .attr("cx",width2)
+    .attr("cy",130)
+    .attr("r", 8)
+    .style("fill", "#0eab00")
+    svg.append("circle")
+    .attr("cx",width2)
+    .attr("cy",160)
+    .attr("r", 8)
+    .style("fill", "#4d91ff")
+    svg.append("circle")
+    .attr("cx",width2)
+    .attr("cy",190)
+    .attr("r", 8)
+    .style("fill", "#ff96e1")
+
+    svg.append("text")
+    .attr("x", width2+15)
+    .attr("y", 130)
+    .text("Total")
+    .style("font-size", "16px")
+    .attr("alignment-baseline","middle")
+    .style("fill", "white")
+    svg.append("text")
+    .attr("x", width2+15)
+    .attr("y", 160)
+    .text("Male")
+    .style("font-size", "16px")
+    .attr("alignment-baseline","middle")
+    .style("fill", "white")
+    svg.append("text")
+    .attr("x", width2+15)
+    .attr("y", 190)
+    .text("Female")
+    .style("font-size", "16px")
+    .attr("alignment-baseline","middle")
+    .style("fill", "white")
+
 }
+
+
+
+
+function parralel(){
+    // set the dimensions and margins of the graph
+    var margin3 = {top: 50, right: 100, bottom: 100, left: 100},
+    w3 = document.getElementById('parralel').offsetWidth;
+    h3 = document.getElementById('parralel').offsetHeight;
+    height3 = h3 - h3*(margin3.top/1000) - h3*(margin3.bottom/1000);
+    width3 = w3 - w3*(margin3.right/1000) - w3*(margin3.left/1000);
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#parralel")
+    .append("svg")
+    .attr("width", width3 + margin3.left + margin3.right)
+    .attr("height", height3 + margin3.top + margin3.bottom)
+    .append("g")
+    .attr("transform",
+            "translate(" + margin3.left + "," + margin3.top + ")");
+
+    // Parse the Data
+    d3.csv("../job_salary_experience.csv", function(data) {
+
+    // Color scale: give me a specie name, I return a color
+    var color = d3.scaleOrdinal()
+        .domain(["Data scientist or machine learning specialist",
+        "Developer  front-end",
+        "Engineer  data",
+        "Engineer  site reliability",
+        "Developer  full-stack",
+        "Developer  back-end",
+        "Developer  desktop or enterprise applications",
+        "Developer  QA or test",
+        "Student",
+        "Developer  mobileEducator",
+        "Engineering manager",
+        "Database administrator",
+        "Academic researcher",
+        "DevOps specialist",
+        "Other",
+        "Developer  embedded applications or devices",
+        "Developer  game or graphics",
+        "Project manager",
+        "Cloud infrastructure engineer",
+        "Data or business analyst",
+        "Designer",
+        "Scientist",
+        "Product manager",
+        "Senior Executive",
+        "System administrator",
+        "Blockchain",
+        "Marketing or sales professional",
+        "Security professional" ])
+        .range(['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+        '#66664D'])
+
+    // Here I set the list of dimension manually to control the order of axis:
+    dimensions = ["avgsalary","experience","jobX"]
+
+    // For each dimension, I build a linear scale. I store all in a y object
+    var y = {"avgsalary":600000,"experience":65,"jobX":26}
+    for (i in dimensions) {
+        name = dimensions[i]
+        y[name] = d3.scaleLinear()
+        .domain( [0,y[name]] ) // --> Same axis range for each group
+        // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
+        .range([height3, 0])
+    }
+
+    // Build the X scale -> it find the best position for each Y axis
+    x = d3.scalePoint()
+        .range([0, width3])
+        .domain(dimensions);
+
+    // Highlight the specie that is hovered
+    var highlight = function(d){
+
+        selected_specie = d.job
+
+        // first every group turns grey
+        d3.selectAll(".line")
+        .transition().duration(200)
+        .style("stroke", "lightgrey")
+        .style("opacity", "0.2")
+        // Second the hovered specie takes its color
+        d3.selectAll("." + selected_specie)
+        .transition().duration(200)
+        .style("stroke", color(selected_specie))
+        .style("opacity", "1")
+    }
+
+    // Unhighlight
+    var doNotHighlight = function(d){
+        d3.selectAll(".line")
+        .transition().duration(200).delay(1000)
+        .style("stroke", function(d){ return( color(d.job))} )
+        .style("opacity", "1")
+    }
+
+    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+    function path(d) {
+        return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+    }
+
+    // Draw the lines
+    svg
+        .selectAll("myPath")
+        .data(data)
+        .enter()
+        .append("path")
+        .attr("class", function (d) { return "line " + d.job } ) // 2 class for each line: 'line' and the group name
+        .attr("d",  path)
+        .style("fill", "none" )
+        .style("stroke", function(d){ return( color(d.job))} )
+        .style("opacity", 0.5)
+        .on("mouseover", highlight)
+        .on("mouseleave", doNotHighlight )
+
+    // Draw the axis:
+    svg.selectAll("myAxis")
+        // For each dimension of the dataset I add a 'g' element:
+        .data(dimensions).enter()
+        .append("g")
+        .attr("class", "axis")
+        // I translate this element to its right position on the x axis
+        .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+        // And I build the axis with the call function
+        .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])); })
+        // Add axis title
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -9)
+        .text(function(d) { return d; })
+        .style("fill", "grey")
+
+    })
+
+    // Title
+    svg.append('text')
+    .attr('x', width/2 + 50)
+    .attr('y', -30)
+    .attr('text-anchor', 'middle')
+    .style('font-family', 'Helvetica')
+    .style('font-size', '24px')
+    .text('Average Salary by Years of Experience and Job')
+    .attr("fill", "white" );
+    
+}
+
+
+
+
+
+function bubble(){
+    // set the dimensions and margins of the graph
+    var margin4 = {top: 100, right: 25, bottom: 100, left: 25},
+    w4 = document.getElementById('bubble').offsetWidth;
+    h4 = document.getElementById('bubble').offsetHeight;
+    height4 = h4 - h4*(margin4.top/1000) - h4*(margin4.bottom/1000);
+    width4 = w4 - w4*(margin4.right/1000) - w4*(margin4.left/1000);
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#bubble")
+    .append("svg")
+    .attr("width", width4 + margin4.left + margin4.right)
+    .attr("height", height4 + margin4.top + margin4.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin4.left + "," + margin4.top + ")");
+
+    //Read the data
+    d3.csv("../job_salary_experience.csv", function(data) {
+
+    // Add X axis
+    var x = d3.scaleLinear()
+    .domain([0, 65])
+    .range([ 0, width4 ]);
+    svg.append("g")
+    .attr("transform", "translate(0," + height4 + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(0,0)")
+    .style("text-anchor", "end")
+    .style('font-size', '12px')
+    .attr("fill", "grey" );
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ height4, 0]);
+    svg.append("g")
+    .call(d3.axisLeft(y))
+    .selectAll("text")
+    .attr("transform", "translate(0,0)")
+    .style("text-anchor", "end")
+    .style('font-size', '12px')
+    .attr("fill", "grey" );
+
+    // Add a scale for bubble size
+    var z = d3.scaleLinear()
+    .domain([0, 750000])
+    .range([ 4, 30]);
+
+    // Add a scale for bubble color
+    var myColor = d3.scaleOrdinal()
+    .domain(["Data scientist or machine learning specialist",
+    "Developer  front-end",
+    "Engineer  data",
+    "Engineer  site reliability",
+    "Developer  full-stack",
+    "Developer  back-end",
+    "Developer  desktop or enterprise applications",
+    "Developer  QA or test",
+    "Student",
+    "Developer  mobileEducator",
+    "Engineering manager",
+    "Database administrator",
+    "Academic researcher",
+    "DevOps specialist",
+    "Other",
+    "Developer  embedded applications or devices",
+    "Developer  game or graphics",
+    "Project manager",
+    "Cloud infrastructure engineer",
+    "Data or business analyst",
+    "Designer",
+    "Scientist",
+    "Product manager",
+    "Senior Executive",
+    "System administrator",
+    "Blockchain",
+    "Marketing or sales professional",
+    "Security professional" ])
+    .range(d3.schemeSet2);
+
+    // Add dots
+    svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return x(d.experience); } )
+    .attr("cy", function (d) { return y(d.jobX); } )
+    .attr("r", function (d) { return z(d.avgsalary); } )
+    .style("fill", function (d) { return myColor(d.job); } )
+    .style("opacity", "0.7")
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
+
+    })
+}
+
+
+
+
+function piechart(){
+
+    
+    var data = [
+        {age: "25-34 years old", number: 28112},
+        {age: "35-44 years old", number: 13988},
+        {age: "Under 18 years old", number: 3866},
+        {age: "18-24 years old", number: 16646},
+        {age: "45-54 years old", number: 5281},
+        {age: "55-64 years old", number: 1978},
+        {age: "65 years or older", number: 554},
+        {age: "Prefer not to say", number: 521},
+    ];
+    var text = "";
+    
+    var width5 = document.getElementById('piechart').offsetWidth - 200;
+    var height5 = document.getElementById('piechart').offsetHeight - 200;
+    var thickness = 40;
+    var duration = 750;
+    var padding = 10;
+    var opacity = .8;
+    var opacityHover = 1;
+    var otherOpacityOnHover = .8;
+    var tooltipMargin = 13;
+    
+    var radius = Math.min(width5-padding, height5-padding) / 2;
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    
+    var svg = d3.select("#piechart")
+    .append('svg')
+    .attr('class', 'pie')
+    .attr('width', width5)
+    .attr('height', height5);
+    
+    var g = svg.append('g')
+    .attr('transform', 'translate(' + (width5/2) + ',' + (height5/2) + ')');
+    
+    var arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
+    
+    var pie = d3.pie()
+    .value(function(d) { return d.number; })
+    .sort(null);
+    
+    var path = g.selectAll('path')
+    .data(pie(data))
+    .enter()
+    .append("g")  
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', (d,i) => color(i))
+    .style('opacity', opacity)
+    .style('stroke', 'white')
+    .on("mouseover", function(d) {
+        d3.selectAll('path')
+            .style("opacity", otherOpacityOnHover);
+        d3.select(this) 
+            .style("opacity", opacityHover);
+    
+        let g = d3.select("svg")
+            .style("cursor", "pointer")
+            .append("g")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+    
+        g.append("text")
+            .attr("class", "name-text")
+            .text(`${d.data.age} (${d.data.number})`)
+            .attr('text-anchor', 'middle');
+        
+        let text = g.select("text");
+        let bbox = text.node().getBBox();
+        let padding = 2;
+        g.insert("rect", "text")
+            .attr("x", bbox.x - padding)
+            .attr("y", height5 + bbox.y - padding)
+            .attr("width", bbox.width + (padding*2))
+            .attr("height", bbox.height + (padding*2))
+            .style("fill", "white")
+            .style("opacity", 0.75);
+        })
+    .on("mousemove", function(d) {
+            let mousePosition = d3.mouse(this);
+            let x = mousePosition[0] + width5;
+            let y = mousePosition[1] + height5 - tooltipMargin;
+        
+            let text = d3.select('.tooltip text');
+            let bbox = text.node().getBBox();
+            if(x - bbox.width/2 < 0) {
+            x = bbox.width/2;
+            }
+            else if(width5 - x - bbox.width/2 < 0) {
+            x = width5 - bbox.width/2;
+            }
+        
+            if(y - bbox.height/2 < 0) {
+            y = bbox.height + tooltipMargin * 2;
+            }
+            else if(height5 - y - bbox.height/2 < 0) {
+            y = height5 - bbox.height/2;
+            }
+        
+            d3.select('.tooltip')
+            .style("opacity", 1)
+            .attr('transform',`translate(${x}, ${y})`);
+        })
+    .on("mouseout", function(d) {   
+        d3.select("svg")
+            .style("cursor", "none")  
+            .select(".tooltip").remove();
+        d3.selectAll('path')
+            .style("opacity", opacity);
+        })
+    .on("touchstart", function(d) {
+        d3.select("svg")
+            .style("cursor", "none");    
+    })
+    .each(function(d, i) { this._current = i; });
+    
+    let legend = d3.select("#piechart").append('div')
+                .attr('class', 'legend')
+                .style('margin-top', '30px');
+    
+    let keys = legend.selectAll('.key')
+                .data(data)
+                .enter().append('div')
+                .attr('class', 'key')
+                .style('display', 'flex')
+                .style('align-items', 'center')
+                .style('margin-right', '20px');
+    
+            keys.append('div')
+                .attr('class', 'symbol')
+                .style('height', '10px')
+                .style('width', '10px')
+                .style('margin', '5px 5px')
+                .style('background-color', (d, i) => color(i));
+    
+            keys.append('div')
+                .attr('class', 'name')
+                .text(d => `${d.age} (${d.number})`);
+    
+            keys.exit().remove();
+}
+
 
 function onChangeSelection(){
     
@@ -244,3 +702,4 @@ function onChangeSelection(){
 
 horizontalBarChart();
 bar_chart_gender();
+piechart();
